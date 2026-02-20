8d6d719d81ba6e44f1202a8a212173f4498510a8
dongeun <dongeun621@naver.com>
2026-02-20 17:53:51 +0900

    2026.02.20 third

--- sw/(02.13) 완전탐색 및 순열 (4)/1. Baby-gin.py
+++ sw/(02.13) 완전탐색 및 순열 (4)/1. Baby-gin.py
@@ -2,3 +2,17 @@ import sys
 
 sys.stdin = open('sample_input1.txt', 'r')
 
+from itertools import combinations, permutations
+T = int(input())
+for tc in range(1, 1+T):
+    arr1 = list(input())
+    arr = []
+    result = False
+    for i in arr1:
+        arr.append(int(i))
+    for i in combinations([0,1,2,3,4,5], 3):
+        first_run = 0
+        first_triplet = 0
+
+
+    print(result)
