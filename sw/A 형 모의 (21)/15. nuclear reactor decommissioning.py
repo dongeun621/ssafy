3297b6451cb76c49f61aeaeb5c4808ef9c9e81f4
dongeun <dongeun621@naver.com>
2026-02-24 14:12:25 +0900

    2026.02.24 second

--- sw/A 형 모의 (21)/15. nuclear reactor decommissioning.py
+++ sw/A 형 모의 (21)/15. nuclear reactor decommissioning.py
@@ -8,12 +8,32 @@ for tc in range(1, T+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
     n_dict = {}
     direct = [[0,1], [0,-1], [-1,0], [1,0]]
+    result = 0
 
     for i, a in enumerate(arr):
-        n_dict[i] = {}
-        n_dict[i]['x'] = a[0]
-        n_dict[i]['y'] = a[1]
-        n_dict[i]['direct'] = a[2]
-        n_dict[i]['k'] = a[3]
+        n_dict[i] = {
+            'x': a[0]*2,
+            'y': a[1]*2,
+            'direct': a[2],
+            'k': a[3]
+        }
 
-    print(n_dict)
+    for _ in range(4000):
+        temp_dict = {}
+        for i, n in n_dict.items():
+            n['x'] += direct[n['direct']][0]
+            n['y'] += direct[n['direct']][1]
+            x = n['x']
+            y = n['y']
+            if (x, y) not in temp_dict:
+                temp_dict[(x, y)] = []
+            temp_dict[(x, y)].append(i)
+
+        for n in temp_dict:
+            if len(temp_dict[n]) > 1:
+                for i in temp_dict[n]:
+                    result += n_dict[i]['k']
+                    n_dict.pop(i)
+
+
+    print(f'#{tc} {result}')
