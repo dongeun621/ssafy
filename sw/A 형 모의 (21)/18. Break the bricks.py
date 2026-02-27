828e4252e6b174d2ce21eb9c1c818d5c0bbc4c1e
dongeun <dongeun621@gmail.com>
2026-02-27 17:14:28 +0900

    2026.02.27 first

--- sw/A 형 모의 (21)/18. Break the bricks.py
+++ sw/A 형 모의 (21)/18. Break the bricks.py
@@ -2,19 +2,41 @@ import sys
 
 sys.stdin = open('sample_input18.txt', 'r')
 
-def bricks(bmap, n):
+dxy = [[0,1], [1,0], [0,-1], [-1,0]]
+
+def bricks(b_map, n):
     global result
     # 남은 횟수 0이면 벽돌개수 세서 비교
     if n == 0:
         cnt = 0
-        for i in range(W):
-            cnt += sum(matrix[i])
+        for a in range(W):
+            cnt += len(b_map[a])
         result = max(result, cnt)
+        return
 
-    for i in range(w):
+    for a in range(W):
+        target = len(b_map[a])-1
+        if not b_map[a]:
+            continue
+        if b_map[a][-1] != 1:
+            new_map = dxy_search(b_map.copy(), a, target)
+            new_map = [[x for x in row if x != 0] for row in new_map]
+        else:
+            new_map = [col[:] for col in b_map]
+            new_map[a].pop()
+        bricks(new_map, n-1)
 
-
-
+def dxy_search(b_map_inner, x, y):
+    K = b_map_inner[x][y]
+    b_map_inner[x][y] = 0
+    if K == 1:
+        return b_map_inner
+    for k in range(1, K):
+        for dx, dy in dxy:
+            xx = x + dx*k
+            yy = y + dy*k
+            if 0 <= x <W and len(b_map_inner[xx]) > y >= 0 and b_map_inner[xx][yy] != 0:
+                b_map_inner = dxy_search(b_map_inner, xx, yy)
 
 T = int(input())
 for tc in range(1, T+1):
@@ -25,6 +47,7 @@ for tc in range(1, T+1):
 
     for i in range(H-1, -1, -1):
         for j in range(W):
-            matrix[j].append(first_matrix[i][j])
+            if first_matrix[i][j] != 0:
+                matrix[j].append(first_matrix[i][j])
 
     bricks(matrix.copy(), N)
