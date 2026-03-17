ef07bb6ee33b8e192c76c821df4798b93befe941
LEEMINGYU <wcentipede27@gmail.com>
2026-03-17 17:51:54 +0900

    2026.03.17 second

--- sw/(03.12) 최단경로(6)/3. minimum cost.py
+++ sw/(03.12) 최단경로(6)/3. minimum cost.py
@@ -2,3 +2,33 @@ import sys
 
 sys.stdin = open('sample_input3.txt', 'r')
 
+from heapq import heappop, heappush
+
+dxy = [(-1,0), (0,1), (1,0), (0,-1)]
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    matrix = [list(map(int,input().split())) for _ in range(N)]
+    min_cost = [[float('inf')]*N for _ in range(N)]
+    min_cost[0][0] = 0
+    heap = [(0, matrix[0][0], 0, 0)]
+
+    while heap:
+        before_cost, before_height, i, j = heappop(heap)
+        for dx, dy in dxy:
+            x = i + dx
+            y = j + dy
+            if 0 <= x < N and 0 <= y < N:
+                after = matrix[x][y]
+                if before_height >= after:
+                    high_d = 1
+                else:
+                    high_d = after - before_height + 1
+                next_cost = before_cost + high_d
+                if min_cost[x][y] > next_cost:
+                    min_cost[x][y] = next_cost
+                    heappush(heap, (next_cost, matrix[x][y], x, y))
+
+
+    print(f'#{tc} {min_cost[N-1][N-1]}')
--- sw/(03.12) 최단경로(6)/4. Insu’s birthday party.py
+++ sw/(03.12) 최단경로(6)/4. Insu’s birthday party.py
@@ -2,3 +2,24 @@ import sys
 
 sys.stdin = open('sample_input4.txt', 'r')
 
+from heapq import heappop, heappush
+
+T = int(input())
+for tc in range(1):
+    N, M, X = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        x, y, c = map(int, input().split())
+        heappush(graph[x],(c,y))
+
+    for i in range(1, N+1):
+        if i == X:
+            continue
+
+        min_cost = [float('inf')]*(N+1)
+        min_cost[i] = 0
+        heap = [(0,c,y) for c, y in graph[i]]
+
+        while heap:
+            before_cost, cost, n = heappop(heap)
+            for c, y in graph[n]:
