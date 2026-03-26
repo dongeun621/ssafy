9534e7f738238860f6b07f87df2251a68f94bd48
신수지 <eunsil5523@gmail.com>
2026-03-26 16:35:32 +0900

    2026.03.26 second

--- 실습/python 문법/5. 데이터 구조1/ws_5_3.py
+++
@@ -1,8 +0,0 @@
-# 아래 함수를 수정하시오.
-def sort_tuple(old_tuple):
-    new_tuple = sorted(old_tuple)
-    return new_tuple
-
-
-result = sort_tuple((5, 2, 8, 1, 3))
-print(result)
--- 실습/python 문법/7. OOP1/ws_7_a.py
+++
@@ -1,9 +0,0 @@
-# 아래에 코드를 작성하시오.
-my_lsit = [1, 2, 3]
-my_dict = {'A': 1, 'B': 2, 'C': 3}
-
-print(dir(my_lsit))
-print(dir(my_dict))
-
-help(my_lsit)
-help(my_dict)
